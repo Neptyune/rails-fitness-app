65ce0b408bc961dc613bb34ba12e6af367eefc2a
Atri Hegde <iamatrihegde@outlook.com>
2023-02-04 20:27:44 +0000

    idk can make user accounts now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,22 @@
 #handle log in log out
 class SessionsController < ApplicationController
-  def new
-    @account = Account.new
+  before_action :authorized
+  helper_method :current_user
+  helper_method :logged_in?
+  def index
+    p params
+    # @account = Account.new
+  end
+
+  def login
+    @params = params
+    @user = Account.find_by(username: params[:username])
+    @valid = @user.authenticate(params[:password])
+    if @valid
+      session[:user_id] = @user.id
+    end
   end
 
   def create
-    @account = Account.new(account_params)
-    if @account.save
-      redirect_to account_url @account
-    else
-      render :new
-    end
   end
 end
