b7045f8354a239f814db7d973af7efb34dd6601a
antoshNikolak <antoninikolak@gmail.com>
2023-02-04 15:36:51 +0000

    added session routes for login and lougout

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'sessions/new'
 
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :accounts
   root "accounts#index"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
