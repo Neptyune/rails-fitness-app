c4696fd9b1b622b43f0609d838e1bbe41273b25b
Atri Hegde <iamatrihegde@outlook.com>
2023-02-05 15:45:55 +0000

    cannot save workout template, id nil

--- app/controllers/workout_templates_controller.rb
+++ app/controllers/workout_templates_controller.rb
@@ -4,6 +4,7 @@ class WorkoutTemplatesController < ApplicationController
   end
 
   def show
+    @workout_template = WorkoutTemplate.find(params[:id])
   end
 
   def new
@@ -15,11 +16,22 @@ class WorkoutTemplatesController < ApplicationController
   end
 
   def create
+    # account_params[:public] = account_params[:public].to_i
+    params[:workout_template][:public] = params[:workout_template][:public].to_i
+    puts(params)
+    puts(account_params)
     @workout_template = WorkoutTemplate.new(account_params)
-    if @workout_template.save
+    puts(@workout_template.name)
+
+    if @workout_template.save!
       redirect_to "/workout_templates", notice: "Workout template was successfully created."
     else
+      raise "workout save failed #{@workout_template.inspect}"
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def account_params
+    params.require(:workout_template).permit(:name, :public, :day_of_week)
   end
 end
